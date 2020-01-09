64a67c6da990fdb30f232de406bda7ab2e0e78c6
负一生韶华 <944081949@qq.com>
2020-01-09 21:35:23 +0800

    未完成

---
+++ src/main/java/org/yjn/mis/model/information.java
@@ -0,0 +1,178 @@
+package org.yjn.mis.model;
+
+import java.util.Date;
+
+public class information {
+    private int id;
+    private String university;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUniversity() {
+        return university;
+    }
+
+    public void setUniversity(String university) {
+        this.university = university;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getMailbox() {
+        return mailbox;
+    }
+
+    public void setMailbox(String mailbox) {
+        this.mailbox = mailbox;
+    }
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String getLab() {
+        return lab;
+    }
+
+    public void setLab(String lab) {
+        this.lab = lab;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public String getEducationExperience() {
+        return educationExperience;
+    }
+
+    public void setEducationExperience(String educationExperience) {
+        this.educationExperience = educationExperience;
+    }
+
+    public String getWorkExperience() {
+        return workExperience;
+    }
+
+    public void setWorkExperience(String workExperience) {
+        this.workExperience = workExperience;
+    }
+
+    public String getResearchDirection() {
+        return researchDirection;
+    }
+
+    public void setResearchDirection(String researchDirection) {
+        this.researchDirection = researchDirection;
+    }
+
+    public String getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String courses) {
+        this.courses = courses;
+    }
+
+    public String getScientificResearchItem() {
+        return scientificResearchItem;
+    }
+
+    public void setScientificResearchItem(String scientificResearchItem) {
+        this.scientificResearchItem = scientificResearchItem;
+    }
+
+    public String getMonograph() {
+        return monograph;
+    }
+
+    public void setMonograph(String monograph) {
+        this.monograph = monograph;
+    }
+
+    public String getPatent() {
+        return patent;
+    }
+
+    public void setPatent(String patent) {
+        this.patent = patent;
+    }
+
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    private String name;
+    private String phoneNumber;
+    private String title;
+    private String mailbox;
+    private String college;
+    private String lab;
+    private String resume;
+    private String educationExperience;
+    private String workExperience;
+    private String researchDirection;
+    private String courses;
+    private String scientificResearchItem;
+    private String monograph;
+    private String patent;
+    private String awards;
+    private Date updateTime;
+    private int state;
+}
